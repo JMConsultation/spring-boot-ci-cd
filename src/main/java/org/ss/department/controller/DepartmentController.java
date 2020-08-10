fbe74390066a865c4fedc5359d546012fed59d55
JMadushan <janithasen@gmail.com>
2020-08-10 17:31:26 +0530

    initial commit

---
+++ src/main/java/org/ss/department/controller/DepartmentController.java
@@ -0,0 +1,15 @@
+package org.ss.department.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<String> getDepartment() {
+        return ResponseEntity.ok("Department for All");
+    }
+}
